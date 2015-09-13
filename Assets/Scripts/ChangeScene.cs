4909480549d244b4000beeefd451edea0575c3b5
Darko Kotoski <wbountyhunter@hotmail.com>
2015-09-13 22:38:49 +0200

    initial commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public GameObject loading;
+	public void ChangeToLevel(string level)
+    {
+        loading.SetActive(true);
+        Application.LoadLevel(level);
+        loading.SetActive(false);
+    }
+}
